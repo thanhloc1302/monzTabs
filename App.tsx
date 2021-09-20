33f52e247d3a6c9a3834b605be011374dba0d40f
thanhloc12 <thanhloc1302@gmail.com>
2021-09-20 23:38:32 +0700

    message2

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+//áds
   if (!isLoadingComplete) {
     return null;
   } else {
